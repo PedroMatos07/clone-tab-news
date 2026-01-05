2bf1d0cc5bb1c43b27414907410beeb762cf5f91
Pedro Matos <pedrom070903@gmail.com>
2026-01-04 22:37:21 -0300

    mudanca na mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Ola Mundo</h1>
+    return <h1>Às vezes a vida vai bater com um tijolo na sua cabeça. Não perca a fé.Tenha coragem de seguir seu coração e sua intuição. Eles já sabem o que você realmente quer se tornar</h1>
 }
 
 export default Home;
