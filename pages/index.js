0cddb25e04e68abb7931026029ea0ac063e38253
Pedro Matos <pedrom070903@gmail.com>
2026-01-09 00:05:37 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home(){
-    return <h1>Às vezes a vida vai bater com um tijolo na sua cabeça. Não perca a fé.Tenha coragem de seguir seu coração e sua intuição. Eles já sabem o que você realmente quer se tornar</h1>
+function Home() {
+  return (
+    <h1>
+      Às vezes a vida vai bater com um tijolo na sua cabeça. Não perca a
+      fé.Tenha coragem de seguir seu coração e sua intuição. Eles já sabem o que
+      você realmente quer se tornar
+    </h1>
+  );
 }
 
+function teste() {
+  console.log("teste");
+  console.log("teste2");
+}
 export default Home;
