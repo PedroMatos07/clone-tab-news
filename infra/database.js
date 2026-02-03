909e89ed5e8a14158c62bbd90daf87f6ad5a08a6
Pedro Matos <pedrom070903@gmail.com>
2026-02-02 21:21:29 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   console.log("Credenciais do Postgres:", {
@@ -27,9 +28,6 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
-
-  await client.end();
-  return result;
 }
 export default {
   query: query,
